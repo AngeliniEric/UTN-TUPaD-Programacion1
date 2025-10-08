ee9aff296743090602b6af503a9009557ab670c7
Eric Angelini <eric_angelini@outlook.com>
2025-10-08 17:44:24 +0000

    Ejercicio 6

--- Estructuras de datos/Trabajo practico 6.py
+++ Estructuras de datos/Trabajo practico 6.py
@@ -38,7 +38,7 @@ print(f"Ejercicio 3: {frutas} \n")
 
 print("Ejercicio 4: \n")
 agenda = {}
-for i in range(5):
+for i in range(1):
     nombre = input("Ingrese el nombre del contacto: ")
     numero = input("Ahora ingrese el número de teléfono: ")
     print("\n")
@@ -47,5 +47,39 @@ buscar = input("Buscar contacto: ")
 if buscar in agenda:
     print(f"Número de teléfono: {agenda[buscar]}")
 else:
-    print("Contacto inexistente. \n")
-    
+    print("Contacto inexistente. ")
+print("\n")
+
+# 5) Solicita al usuario una frase e imprime:
+# • Las palabras únicas (usando un set).
+# • Un diccionario con la cantidad de veces que aparece cada palabra.
+
+print("Ejercicio 5: \n")
+frase = input("Ingrese una frase: ")
+frase = frase.lower()
+palabras = frase.split()
+palabras_unicas = set(palabras)
+print(f"Palabras unicas: {palabras_unicas}")
+palabra_cantidad = {}
+for palabra in palabras:
+    if palabra in palabra_cantidad:
+        palabra_cantidad[palabra] += 1
+    else: 
+        palabra_cantidad[palabra] = 1
+print(f"{palabra_cantidad}")
+print("\n")
+
+# 6) Permití ingresar los nombres de 3 alumnos, y para cada uno una tupla de 3 notas.
+# Luego, mostrá el promedio de cada alumno.
+
+print("Ejercicio 6: \n")
+datos_alumnos = {}
+for i in range(3):
+    alumno = input(f"Ingrese el nombre del alumno n°{i+1}: ")
+    notas = []
+    for j in range(3):
+        nota = int(input(f"Ingrese la nota {j+1} de {alumno}: "))
+        notas.append(nota)
+    tupla_notas = tuple(notas)
+    datos_alumnos[alumno] = tupla_notas
+print(f"Registro completo: {datos_alumnos}")
